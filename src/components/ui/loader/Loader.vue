b6c0e0209e9ca6a49bcf5b112605152c71c3fd91
irfan <apipiirpan@gmail.com>
2025-11-06 20:50:00 +0700

    Refactor Loader component to include height for better layout control

--- src/components/ui/loader/Loader.vue
+++ src/components/ui/loader/Loader.vue
@@ -1,9 +1,9 @@
 <template>
   <div class="fixed inset-0 bg-[#1b1b1b] bg-opacity-80 flex items-center justify-center z-50">
-    <div class="relative w-[177px]">
-      <div class="absolute bg-[#d9d9d9] h-px w-full top-1/2 -translate-y-1/2"></div>
-      <div class="absolute bg-[#cb8a2e] h-[3px] w-[90px] top-1/2 -translate-y-1/2 animate-loading"></div>
-    </div>
+      <div class="relative w-[177px] h-[5px] overflow-hidden">
+        <div class="absolute bg-[#d9d9d9] h-px w-full top-1/2 -translate-y-1/2"></div>
+        <div class="absolute bg-[#cb8a2e] h-[3px] w-[90px] top-1/2 -translate-y-1/2 animate-loading"></div>
+      </div>
   </div>
 </template>
 
